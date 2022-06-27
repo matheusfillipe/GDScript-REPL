77cca016bcc0c8da6605845ec74e32f33088a80e
Matheus Fillipe <matheusfillipeag@gmail.com>
2022-06-27 07:05:52 +0000

    Added instructions

--- README.md
+++ README.md
@@ -1,5 +1,90 @@
+# GDScript REPL
 
-#### Why the dockerfile doesn't download godot binary automatically?
+This project contains:
+
+- A simple docker file to build godot server for alpine
+- A dockerfile to run godot from alpine
+- A proof of concept gdscript REPL
+- A IRC gdscript REPL bot 
+
+## Motivation
+
+Gdscript is a python like language but it lacks a reply. Godot has a built in `godot -s script.gd` to run scripts but it is overkill when you just want to test out the difference between a `PoolStringArray` and a normal `Array` of strings and play around like you can do with so many languages.
+
+That inspired me to try to turn `godot -s` into a repl, creating a [websocket server](https://docs.godotengine.org/en/stable/classes/class_websocketserver.html) that will take any string from any client in, evaluate it by creating new [GDScript](https://docs.godotengine.org/en/stable/classes/class_script.html), attaching that script to a resource node and then calling a function of that node. That requires a lot of hacky string manipulations to keep stuff working and have a separated local and global scopes allowing you to create functions, enums and classes from the repl. 
+
+This is this still very work in progress and experimental but serves to prove the point that a repl for godot would be awesome.
+
+## Usage
+
+The gdscript server is implemented in a way that it will send the return output to the client but not stdout. So if you type `1+1` you will receive `2` but you can't receive `print(2)` event though that will be still shown on the server's output.
+
+### CLI
+
+Requires python3
+
+1. Copy `config.py.example` to `config.py`
+2. Edit it for your needs. Ubuntu has `sudo apt install godot3-server` which is very suitable for this. In another distros without that you might want to change `GODOT` to `godot --no-window` to run it headlessly.
+3. You can create a virtual environment or not: `pip3 install requirements.py`  
+4. Run `./main.py`
+
+With this you will see both stdout and return output in the same window.
+
+### Server
+
+Run the godot script:
+```bash
+godot3-server --script gdserver.gd
+# or
+godot --no-window --script gdserver.gd
+```
+
+You can connect to this server using any websocket client. I recommend [websocat](https://github.com/vi/websocat):
+
+```bash
+websocat ws://127.0.0.1:9080
+
+# Or if you have rlwrap installed (You should)
+rlwrap websocat ws://127.0.0.1:9080
+```
+
+The problem with this is that stdout and stderr will be displayed on the server while only the return will be shown on the client.
+
+## Environtment variables
+
+If `DEBUG=1` is set then the server will keep writing the formed script to stdout.
+
+If `TEST=1` the websocket serevr wont run and simple test functions will be executed.
+
+### Why the weird approach
+
+My main goal was to make a safe to host irc bot repl, spawning a docker image for each command. The `OS` module contains dangerous functions that allow you to run shell commands. In that process I realized it would be easy to make a normal CLI repl as well.
+
+## Run the IRC bot
+
+Requires python3
+
+1. Build a docker image for it (See section bellow).
+2. Copy bot_`config.py.example` to bot_`config.py`
+3. Edit it for your needs. 
+4. You can create a virtual environment or not: `pip3 install bot_requirements.py`  
+5. Run `./bot.py`
+
+
+
+## Docker
+
+### Build GODOT
+
+To build godot for your platform run  `docker-compose build` inside the `build/` directory. Then run `./copy.sh` and the resulting binary will be inside `build/bin/`.
+
+This was only tested on AARCH64. If you want to run the irc bot in a supported platform simply grab godot-server from https://godotengine.org/download/server
+
+### Build and image for the irc bot
+
+If you build godot, put the binary inside the `docker` folder and rename it to `godot`. Then inside `docker/` run `docker-compose build` and you will have it. You can also use the same docker image you build it from, just update the bot config accordingly.
+
+### Why the dockerfile doesn't download godot binary automatically?
 
 Because I hate when I try to use one of those docker images but they are x86_64 only. Godot is fairly easy to build.
 If you are on aarch64 like me (Raspberry Pi 4, oracle Ampere A1) this is how I build godot:
---
+++ bot.py
@@ -0,0 +1,179 @@
+import os
+import re
+import signal
+import threading
+from pathlib import Path
+from tempfile import NamedTemporaryFile
+
+import requests
+import trio
+from cachetools import TTLCache
+from IrcBot.bot import IrcBot, Message, utils
+from pexpect import replwrap
+
+from bot_config import (CHANNELS, COQ_REPL_TTL, COQTOP_CMD, NICK, PORT, PREFIX,
+                  SERVER, SSL)
+from message_server import listen_loop
+
+user_repls = TTLCache(maxsize=5, ttl=COQ_REPL_TTL)
+user_history = TTLCache(maxsize=32, ttl=COQ_REPL_TTL)
+
+utils.setHelpHeader(
+    "USE: {PREFIX} [coq command here]       - (Notice the space)")
+utils.setHelpBottom(
+    "Nice tutorial coq at https://learnxinyminutes.com/docs/coq/")
+utils.setLogging(10)
+utils.setParseOrderTopBottom(True)
+utils.setPrefix(PREFIX)
+
+info = utils.log
+
+FIFO = NamedTemporaryFile(mode='w+b', prefix='coq-repl-',
+                          suffix='.fifo', delete=False).name
+COQ_EXPORT_DIR = "./coq-export/"
+
+# If export dir for coq doesn't exist, create it
+if not os.path.exists(COQ_EXPORT_DIR):
+    os.makedirs(COQ_EXPORT_DIR)
+
+# Change working directory to the directory of COQ_EXPORT_DIR
+os.chdir(COQ_EXPORT_DIR)
+
+def ansi2irc(text):
+    """Convert ansi colors to irc colors."""
+    text = re.sub(r'\x1b\[([0-9;]+)m', lambda m: '\x03' + m.group(1), text)
+    text = re.sub(r'\x1b\[([0-9;]+)[HJK]', lambda m: '\x1b[%s%s' %
+                  (m.group(1), m.group(2)), text)
+    return text
+
+
+def reply(msg: Message, text: str):
+    """Reply to a message."""
+    with open(FIFO, "w") as f:
+        for line in text.splitlines():
+            if not line.strip():
+                continue
+            line = f"<{msg.nick}> {ansi2irc(line)}"
+            f.write(f"[[{msg.channel}]] {line}\n")
+
+
+def paste(text):
+    """Paste text to ix.io."""
+    info(f"Pasting {text=}")
+    try:
+        url = "http://ix.io"
+        payload = {'f:1=<-': text}
+        response = requests.request("POST", url, data=payload)
+        return response.text
+    except Exception as e:
+        info(f"Error {e=}")
+        return "Failed to paste"
+
+
+def read_paste(url):
+    """Read text from ix.io."""
+    response = requests.request("GET", url)
+    return response.text
+
+
+def run_command(msg: Message, text: str):
+
+    def _run_command(msg: Message, text: str):
+        def __run_command(msg: Message, text: str):
+            global user_repls, user_history
+            user = msg.nick
+            if user not in user_repls:
+                info(f"Creating new repl for {user}")
+                user_repls[user] = replwrap.REPLWrapper(
+                    COQTOP_CMD, "Coq <", prompt_change=None)
+            reply(msg, user_repls[user].run_command(text, timeout=2))
+
+            if user not in user_history:
+                user_history[user] = []
+            user_history[user].append(text)
+
+        t = threading.Thread(target=__run_command,
+                             args=(msg, text), daemon=True)
+        t.start()
+        t.join(2)
+        if t.is_alive():
+            coqtop: replwrap.REPLWrapper = user_repls[msg.nick]
+            coqtop.child.kill(signal.SIGINT)
+            user_repls.pop(msg.nick, None)
+            reply(msg, "Command timed out. I Cleared your environment")
+
+    threading.Thread(target=_run_command, args=(msg, text)).start()
+
+
+@utils.arg_command("clear", "Clear environment")
+async def clear(bot: IrcBot, match: re.Match, message: Message):
+    global user_repls
+    user_repls.pop(message.nick, None)
+    user_history.pop(message.nick, None)
+    reply(message, "Environment cleared")
+
+
+@utils.regex_cmd_with_messsage(f"^{PREFIX} (.+)$")
+async def run(bot: IrcBot, match: re.Match, message: Message):
+    text = match.group(1).strip()
+    run_command(message, text)
+
+
+@utils.arg_command("paste", "Pastes your environment code")
+async def pipaste(bot: IrcBot, args: re.Match, msg: Message):
+    if msg.nick not in user_repls:
+        reply(msg, "You don't have an environment")
+        return
+    reply(msg, paste("\n".join(user_history[msg.nick])))
+
+
+@utils.arg_command("read", "Populates your environment code with code from url")
+async def readurl(bot: IrcBot, args: re.Match, msg: Message):
+    if not args[1]:
+        reply(msg, "Please provide a url")
+        return
+    try:
+        run_command(msg, read_paste(args[1]))
+    except Exception as e:
+        reply(msg, "Failed to read paste: " + str(e))
+    reply(msg, "Code has been read and sent!")
+
+
+async def onConnect(bot: IrcBot):
+    for channel in CHANNELS:
+        await bot.join(channel)
+
+    async def message_handler(text):
+        for line in text.splitlines():
+            match = re.match(r"^\[\[([^\]]+)\]\] (.*)$", line)
+            if match:
+                channel, text = match.groups()
+                await bot.send_message(text, channel)
+
+    async def update_loop():
+        """Update cache to eliminate invalid keys and monitor COQ_EXPORT_DIR."""
+        global user_repls, user_history
+        while True:
+            user_repls.pop(None, None)
+            user_history.pop(None, None)
+            for file in Path("./").glob("*"):
+                info(f"Found {file=}")
+                if not file.is_file():
+                    continue
+                name = file.name
+                url = paste(file.read_text())
+
+                # TODO what channel should we send this to?
+                for channel in CHANNELS:
+                    await bot.send_message(f"{name}: {url}", channel)
+                file.unlink()
+
+            await trio.sleep(3)
+
+    async with trio.open_nursery() as nursery:
+        nursery.start_soon(listen_loop, FIFO, message_handler)
+        nursery.start_soon(update_loop)
+
+if __name__ == "__main__":
+    bot = IrcBot(SERVER, PORT, NICK, use_ssl=SSL)
+    bot.runWithCallback(onConnect)
